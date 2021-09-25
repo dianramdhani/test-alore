502aa034694bd772a3c135a111bd7ea250193d36
dianramdhani <dian.ramdhani@gmail.com>
2021-09-25 23:13:07 +0700

    9 files modified, 6 deleted

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {} from '@angular/material';
 import { EmojiModule } from '@ctrl/ngx-emoji-mart/ngx-emoji';
 import { PickerModule } from '@ctrl/ngx-emoji-mart';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SidemenuComponent } from './components/sidemenu/sidemenu.component';
+import { MatInputModule } from '@angular/material/input/typings/input-module';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, MatInputModule],
   declarations: [NavbarComponent, SidemenuComponent],
   exports: [NavbarComponent, SidemenuComponent, EmojiModule, PickerModule],
 })
